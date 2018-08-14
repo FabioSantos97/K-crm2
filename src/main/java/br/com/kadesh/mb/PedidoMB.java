c52c74c4bc61cdc731c5e75e432252ba76c17e5e
unknown <rubem.caggiano@gmail.com>
2018-08-13 22:16:09 -0300

    Correção event onclick

--- src/main/java/br/com/kadesh/mb/PedidoMB.java
+++ src/main/java/br/com/kadesh/mb/PedidoMB.java
@@ -31,7 +31,7 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class PedidoMB implements Serializable {
-    
+
     private PedidoDao pedidoDao = new PedidoDao();
     private ClienteDao clienteDao = new ClienteDao();
     private CondPagDao condPagDao = new CondPagDao();
@@ -42,7 +42,7 @@ public class PedidoMB implements Serializable {
     private ProdutoDao produtoDao = new ProdutoDao();
     private ProdutoGradeDao produtoGradeDao = new ProdutoGradeDao();
     private OpcionaisDao opcionaisDao = new OpcionaisDao();
-    
+
     private List<TipoPedido> tipoPedidos;
     private List<Transportadora> transportadoras;
     private List<CondicaoPagamento> condicoes;
@@ -54,7 +54,7 @@ public class PedidoMB implements Serializable {
     private List<GradeVenda> gradeVendas = new ArrayList<>();
     private List<ProdutoGrade> produtosGrade;
     private List<Opcional> opcionaisDisponiveis;
-    
+
     private Pedido pedido;
     private Cliente cliente;
     private CondicaoPagamento condicaoPagamento;
@@ -67,17 +67,18 @@ public class PedidoMB implements Serializable {
     private ItemPedido itemPedido;
     private GradeVenda gradeVenda;
     private Opcional opcional;
-    
+
     public PedidoMB() {
         selectAll();
         pedido = new Pedido();
-        
+
         endereco = new Endereco();
+        estado = new Estado();
         produtoGrade = new ProdutoGrade();
         itemPedido = new ItemPedido();
         gradeVenda = new GradeVenda();
     }
-    
+
     public void selectAll() {
         pedidos = pedidoDao.findAll();
         clientes = clienteDao.findAll();
@@ -85,43 +86,43 @@ public class PedidoMB implements Serializable {
         transportadoras = transportadoraDao.findAll();
         tipoPedidos = tipoPedidoDao.findAll();
         estados = estadoDao.findAll();
-        
+
         produtos = produtoDao.findAll();
-        
+
         opcionaisDisponiveis = opcionaisDao.findAll();
-        
+
     }
-    
+
     public void adicionarGrade() {
         gradeVenda.setProduto(produto);
         gradeVenda.setGrade(produtoGrade);
-        
+
         gradeVendas.add(gradeVenda);
-        
+
         gradeVenda = new GradeVenda();
     }
-    
+
     public void adicionarItem() {
         int quantidade = 0;
         itemPedido.setProdutos(gradeVendas);
         itemPedido.setProduto(produto);
-        
+
         for (GradeVenda gv : gradeVendas) {
-            
+
             quantidade = quantidade + gv.getQuantidade();
         }
-        
+
         itemPedido.setQuantidade(quantidade);
         itens.add(itemPedido);
-        
+
         itemPedido = new ItemPedido();
         gradeVendas = new ArrayList<>();
         produto = new Produto();
         gradeVenda = new GradeVenda();
-        
+
         int quantidadeTotal = 0;
         double valorTotal = 0;
-        
+
         for (ItemPedido iP : itens) {
             quantidadeTotal = quantidadeTotal + iP.getQuantidade();
             valorTotal = valorTotal + iP.getValorItens();
@@ -129,17 +130,17 @@ public class PedidoMB implements Serializable {
         pedido.setQuantidade(quantidadeTotal);
         pedido.setValorTotal(valorTotal);
     }
-    
+
     public void carregarProdutos() {
         produtos = produtoDao.findAll();
-        
+
     }
-    
+
     public void carregarEndereco() {
         endereco = cliente.getEndereco();
-//        estado = cliente.getEndereco().getEstado();
+        estado = endereco.getEstado();
     }
-    
+
     public void salvar() {
         System.out.println("Salvo1");
         pedido.setTipoPedido(tipoPedido);
@@ -148,286 +149,288 @@ public class PedidoMB implements Serializable {
         pedido.setCondicaoPagamento(condicaoPagamento);
         pedido.setTransportadora(transportadora);
         pedido.setItensPedido(itens);
-        
+
         pedidoDao.saveOrUpdate(pedido);
         System.out.println("Salvo2");
     }
-    
+
     public void removerItemGrade(GradeVenda gradeVenda) {
         gradeVendas.remove(gradeVenda);
     }
-    
+
     public void removerItem() {
-        
+
     }
-    
+
     public void editarItem() {
-        
+
     }
-    
+
     public TransportadoraDao getTransportadoraDao() {
         return transportadoraDao;
     }
-    
+
     public void setTransportadoraDao(TransportadoraDao transportadoraDao) {
         this.transportadoraDao = transportadoraDao;
     }
-    
+
     public TipoPedidoDao getTipoPedidoDao() {
         return tipoPedidoDao;
     }
-    
+
     public void setTipoPedidoDao(TipoPedidoDao tipoPedidoDao) {
         this.tipoPedidoDao = tipoPedidoDao;
     }
-    
+
     public List<TipoPedido> getTipoPedidos() {
         return tipoPedidos;
     }
-    
+
     public void setTipoPedidos(List<TipoPedido> tipoPedidos) {
         this.tipoPedidos = tipoPedidos;
     }
-    
+
     public List<Transportadora> getTransportadoras() {
         return transportadoras;
     }
-    
+
     public void setTransportadoras(List<Transportadora> transportadoras) {
         this.transportadoras = transportadoras;
     }
-    
+
     public CondPagDao getCondPagDao() {
         return condPagDao;
     }
-    
+
     public void setCondPagDao(CondPagDao condPagDao) {
         this.condPagDao = condPagDao;
     }
-    
+
     public List<CondicaoPagamento> getCondicoes() {
         return condicoes;
     }
-    
+
     public void setCondicoes(List<CondicaoPagamento> condicoes) {
         this.condicoes = condicoes;
     }
-    
+
     public PedidoDao getPedidoDao() {
         return pedidoDao;
     }
-    
+
     public void setPedidoDao(PedidoDao pedidoDao) {
         this.pedidoDao = pedidoDao;
     }
-    
+
     public List<Pedido> getPedidos() {
         return pedidos;
     }
-    
+
     public void setPedidos(List<Pedido> pedidos) {
         this.pedidos = pedidos;
     }
-    
+
     public Pedido getPedido() {
         return pedido;
     }
-    
+
     public void setPedido(Pedido pedido) {
         this.pedido = pedido;
     }
-    
+
     public ClienteDao getClienteDao() {
         return clienteDao;
     }
-    
+
     public void setClienteDao(ClienteDao clienteDao) {
         this.clienteDao = clienteDao;
     }
-    
+
     public List<Cliente> getClientes() {
         return clientes;
     }
-    
+
     public void setClientes(List<Cliente> clientes) {
         this.clientes = clientes;
     }
-    
+
     public Cliente getCliente() {
         return cliente;
+        
     }
-    
+
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+        carregarEndereco();
     }
-    
+
     public CondicaoPagamento getCondicaoPagamento() {
         return condicaoPagamento;
     }
-    
+
     public void setCondicaoPagamento(CondicaoPagamento condicaoPagamento) {
         this.condicaoPagamento = condicaoPagamento;
     }
-    
+
     public Transportadora getTransportadora() {
         return transportadora;
     }
-    
+
     public void setTransportadora(Transportadora transportadora) {
         this.transportadora = transportadora;
     }
-    
+
     public TipoPedido getTipoPedido() {
         return tipoPedido;
     }
-    
+
     public void setTipoPedido(TipoPedido tipoPedido) {
         this.tipoPedido = tipoPedido;
     }
-    
+
     public EnderecoDao getEnderecoDao() {
         return enderecoDao;
     }
-    
+
     public void setEnderecoDao(EnderecoDao enderecoDao) {
         this.enderecoDao = enderecoDao;
     }
-    
+
     public Endereco getEndereco() {
         return endereco;
     }
-    
+
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
-    
+
     public List<Estado> getEstados() {
         return estados;
     }
-    
+
     public void setEstados(List<Estado> estados) {
         this.estados = estados;
     }
-    
+
     public EstadoDao getEstadoDao() {
         return estadoDao;
     }
-    
+
     public void setEstadoDao(EstadoDao estadoDao) {
         this.estadoDao = estadoDao;
     }
-    
+
     public Estado getEstado() {
         return estado;
     }
-    
+
     public void setEstado(Estado estado) {
         this.estado = estado;
     }
-    
+
     public ProdutoDao getProdutoDao() {
         return produtoDao;
     }
-    
+
     public void setProdutoDao(ProdutoDao produtoDao) {
         this.produtoDao = produtoDao;
     }
-    
+
     public List<Produto> getProdutos() {
         return produtos;
     }
-    
+
     public void setProdutos(List<Produto> produtos) {
         this.produtos = produtos;
     }
-    
+
     public Produto getProduto() {
         return produto;
     }
-    
+
     public void setProduto(Produto produto) {
         this.produto = produto;
         produtosGrade = this.produto.getNumeracao();
     }
-    
+
     public List<ItemPedido> getItens() {
         return itens;
     }
-    
+
     public void setItens(List<ItemPedido> itens) {
         this.itens = itens;
     }
-    
+
     public ProdutoGrade getProdutoGrade() {
         return produtoGrade;
     }
-    
+
     public void setProdutoGrade(ProdutoGrade produtoGrade) {
         this.produtoGrade = produtoGrade;
     }
-    
+
     public ItemPedido getItemPedido() {
         return itemPedido;
     }
-    
+
     public void setItemPedido(ItemPedido itemPedido) {
         this.itemPedido = itemPedido;
     }
-    
+
     public List<GradeVenda> getGradeVendas() {
         return gradeVendas;
     }
-    
+
     public void setGradeVendas(List<GradeVenda> gradeVendas) {
         this.gradeVendas = gradeVendas;
     }
-    
+
     public GradeVenda getGradeVenda() {
         return gradeVenda;
     }
-    
+
     public void setGradeVenda(GradeVenda gradeVenda) {
         this.gradeVenda = gradeVenda;
     }
-    
+
     public ProdutoGradeDao getProdutoGradeDao() {
         return produtoGradeDao;
     }
-    
+
     public void setProdutoGradeDao(ProdutoGradeDao produtoGradeDao) {
         this.produtoGradeDao = produtoGradeDao;
     }
-    
+
     public List<ProdutoGrade> getProdutosGrade() {
         return produtosGrade;
     }
-    
+
     public void setProdutosGrade(List<ProdutoGrade> produtosGrade) {
         this.produtosGrade = produtosGrade;
     }
-    
+
     public OpcionaisDao getOpcionaisDao() {
         return opcionaisDao;
     }
-    
+
     public void setOpcionaisDao(OpcionaisDao opcionaisDao) {
         this.opcionaisDao = opcionaisDao;
     }
-    
+
     public List<Opcional> getOpcionaisDisponiveis() {
         return opcionaisDisponiveis;
     }
-    
+
     public void setOpcionaisDisponiveis(List<Opcional> opcionaisDisponiveis) {
         this.opcionaisDisponiveis = opcionaisDisponiveis;
     }
-    
+
     public Opcional getOpcional() {
         return opcional;
     }
-    
+
     public void setOpcional(Opcional opcional) {
         this.opcional = opcional;
     }
-    
+
 }
